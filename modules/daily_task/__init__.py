22a560824d7070ac84ae1ebcbc3899d2af12f4b0
rumesh <rumesh.m.s16@gmail.com>
2020-07-02 08:57:02 +0530

    scheduler

--- modules/daily_task/__init__.py
+++ modules/daily_task/__init__.py
@@ -1,23 +1,34 @@
-import time
 import atexit
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
+from modules.company import Company
+from modules.config import Config
+
 
 class DailyTask:
     def __init__(self):
-        self.companies = ["FB", "AAPL", "NFLX", "GOOG"]
+        self.companies = Config.COMPANIES
 
-    # get data from api and save todo
+    # get data from api and save
     def task(self):
-        print("Task is running")
+        print("Scheduled task is running")
+        try:
+            for comp in self.companies:
+                c = Company(comp)
+                c.set_recommendations()
+        except Exception as e:
+            print("Error in Task", e)
 
     def start(self):
-        scheduler = BackgroundScheduler()
-        scheduler.add_job(func=self.task, trigger="interval", seconds=3)
-        scheduler.start()
-        # Shut down the scheduler when exiting the app
-        atexit.register(lambda: scheduler.shutdown())
+        try:
+            scheduler = BackgroundScheduler()
+            scheduler.add_job(func=self.task, trigger="interval", seconds=5)
+            scheduler.start()
+            # Shut down the scheduler when exiting the app
+            atexit.register(lambda: scheduler.shutdown())
+        except Exception as e:
+            print("Error in Job Scheduler", e)
 
 
 if __name__ == '__main__':
